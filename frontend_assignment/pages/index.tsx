b56488fd62595361e5aa21be885b7f4f24e227d2
Toki <gorjan4ilijevski@gmail.com>
2022-06-02 01:46:49 +0200

    This is the week4 code

--- frontend_assignment/pages/index.tsx
+++ frontend_assignment/pages/index.tsx
@@ -1,83 +1,168 @@
-import detectEthereumProvider from "@metamask/detect-provider"
-import { Strategy, ZkIdentity } from "@zk-kit/identity"
-import { generateMerkleProof, Semaphore } from "@zk-kit/protocols"
-import { providers } from "ethers"
-import Head from "next/head"
-import React from "react"
-import styles from "../styles/Home.module.css"
+import detectEthereumProvider from "@metamask/detect-provider";
+import { Strategy, ZkIdentity } from "@zk-kit/identity";
+import { generateMerkleProof, Semaphore } from "@zk-kit/protocols";
+import Head from "next/head";
+import React, { useState } from "react";
+import styles from "../styles/Home.module.css";
+import TextField from "@mui/material/TextField";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import Greeter from "artifacts/contracts/Greeters.sol/Greeters.json";
+import TextBox from "./textbox"; 
+import { providers } from "ethers";
 
 export default function Home() {
-    const [logs, setLogs] = React.useState("Connect your wallet and greet!")
+  
+  const [logs, setLogs] = React.useState("Connect your wallet and greet!");
 
-    async function greet() {
-        setLogs("Creating your Semaphore identity...")
 
-        const provider = (await detectEthereumProvider()) as any
+  const validationSchema = Yup.object().shape({
+    name: Yup.string()
+      .required("Fullname is required")
+      .min(6, "Name must be at least 6 characters")
+      .max(20, "Name must not exceed 20 characters"),
+    age: Yup.number().required("Age is required"),
+    address: Yup.string().required("Address is required"),
+  });
+  const formik = useFormik({
+    initialValues: { name: "", age: "", address: "" },
+    validationSchema,
+    onSubmit: (data: any) => {
+      console.log(JSON.stringify(data, null, 2));
+      greet();
+    },
+  });
 
-        await provider.request({ method: "eth_requestAccounts" })
+  async function greet() {
+    setLogs("Creating your Semaphore identity...");
 
-        const ethersProvider = new providers.Web3Provider(provider)
-        const signer = ethersProvider.getSigner()
-        const message = await signer.signMessage("Sign this message to create your identity!")
+    const provider = (await detectEthereumProvider()) as any;
 
-        const identity = new ZkIdentity(Strategy.MESSAGE, message)
-        const identityCommitment = identity.genIdentityCommitment()
-        const identityCommitments = await (await fetch("./identityCommitments.json")).json()
+    await provider.request({ method: "eth_requestAccounts" });
 
-        const merkleProof = generateMerkleProof(20, BigInt(0), identityCommitments, identityCommitment)
+    const ethersProvider = new providers.Web3Provider(provider);
+    const signer = ethersProvider.getSigner();
+    const message = await signer.signMessage(
+      "Sign this message to create your identity!"
+    );
 
-        setLogs("Creating your Semaphore proof...")
+    const identity = new ZkIdentity(Strategy.MESSAGE, message);
+    const identityCommitment = identity.genIdentityCommitment();
+    const identityCommitments = await (
+      await fetch("./identityCommitments.json")
+    ).json();
 
-        const greeting = "Hello world"
+    const merkleProof = generateMerkleProof(
+      20,
+      BigInt(0),
+      identityCommitments,
+      identityCommitment
+    );
 
-        const witness = Semaphore.genWitness(
-            identity.getTrapdoor(),
-            identity.getNullifier(),
-            merkleProof,
-            merkleProof.root,
-            greeting
-        )
+    setLogs("Creating your Semaphore proof...");
 
-        const { proof, publicSignals } = await Semaphore.genProof(witness, "./semaphore.wasm", "./semaphore_final.zkey")
-        const solidityProof = Semaphore.packToSolidityProof(proof)
+    const greeting = "Hello world";
 
-        const response = await fetch("/api/greet", {
-            method: "POST",
-            body: JSON.stringify({
-                greeting,
-                nullifierHash: publicSignals.nullifierHash,
-                solidityProof: solidityProof
-            })
-        })
+    const witness = Semaphore.genWitness(
+      identity.getTrapdoor(),
+      identity.getNullifier(),
+      merkleProof,
+      merkleProof.root,
+      greeting
+    );
 
-        if (response.status === 500) {
-            const errorMessage = await response.text()
+    const { proof, publicSignals } = await Semaphore.genProof(
+      witness,
+      "./semaphore.wasm",
+      "./semaphore_final.zkey"
+    );
+    const solidityProof = Semaphore.packToSolidityProof(proof);
 
-            setLogs(errorMessage)
-        } else {
-            setLogs("Your anonymous greeting is onchain :)")
-        }
+    const response = await fetch("/api/greet", {
+      method: "POST",
+      body: JSON.stringify({
+        greeting,
+        nullifierHash: publicSignals.nullifierHash,
+        solidityProof: solidityProof,
+      }),
+    });
+
+    if (response.status === 500) {
+      const errorMessage = await response.text();
+
+      setLogs(errorMessage);
+    } else {
+      setLogs("Your anonymous greeting is onchain :)");
     }
+  }
 
-    return (
-        <div className={styles.container}>
-            <Head>
-                <title>Greetings</title>
-                <meta name="description" content="A simple Next.js/Hardhat privacy application with Semaphore." />
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Greetings</title>
+        <meta
+          name="description"
+          content="A simple Next.js/Hardhat privacy application with Semaphore."
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
-            <main className={styles.main}>
-                <h1 className={styles.title}>Greetings</h1>
+      <main className={styles.main}>
+        <h1 className={styles.title}>Greetings</h1>
 
-                <p className={styles.description}>A simple Next.js/Hardhat privacy application with Semaphore.</p>
+        <p className={styles.description}>
+          A simple Next.js/Hardhat privacy application with Semaphore.
+        </p>
+        <form onSubmit={formik.handleSubmit}>
+          <TextField
+            id="name"
+            color="warning"
+            label="Name"
+            focused
+            name="name"
+            style={{ margin: 30 }}
+            onChange={formik.handleChange}
+            value={formik.values.name}
+          />
+          <div style={{ color: "red" }}>
+            {formik.errors.name ? formik.errors.name : null}
+          </div>
+          <TextField
+            id="age"
+            color="warning"
+            label="age"
+            name="age"
+            focused
+            style={{ margin: 30 }}
+            onChange={formik.handleChange}
+            value={formik.values.age}
+          />
+          <div style={{ color: "red" }}>
+            {formik.errors.age ? formik.errors.age : null}
+          </div>
+          <TextField
+            id="address"
+            name="address"
+            color="warning"
+            label="Address"
+            focused
+            style={{ margin: 30 }}
+            onChange={formik.handleChange}
+            value={formik.values.address}
+          />
+          <div>
+          <TextBox value={"emiting event here"}/>
+          </div>
+          <div style={{ color: "red" }}>
+            {formik.errors.address ? formik.errors.address : null}
+          </div>
+          <div className={styles.logs}>{logs}</div>
 
-                <div className={styles.logs}>{logs}</div>
-
-                <div onClick={() => greet()} className={styles.button}>
-                    Greet
-                </div>
-            </main>
-        </div>
-    )
+          <button type="submit" className={styles.button}>
+            Greet
+          </button>
+        </form>
+      </main>
+    </div>
+  );
 }
